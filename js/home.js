20b2244f6d6500eaf99ed2fc2daf44322e39be04
Naz-mul11 <ni2336282@gmail.com>
2024-09-24 21:14:58 +0600

    added the html,java and assets folder

---
+++ js/home.js
@@ -0,0 +1,7 @@
+document.getElementById('add-money')
+.addEventListener('click', function(event){
+    event.preventDefault();
+
+    const addMonyInput = document.getElementById('input-add-money').value;
+
+});
