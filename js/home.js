3dfdcd40afc277073c4a6fde1540675f6fa64569
Naz-mul11 <ni2336282@gmail.com>
2024-09-25 10:51:13 +0600

    added the html and java script code

--- js/home.js
+++ js/home.js
@@ -3,5 +3,22 @@ document.getElementById('add-money')
     event.preventDefault();
 
     const addMonyInput = document.getElementById('input-add-money').value;
+    
+
+    if (addMonyInput || number <= 0) {
+        alert('add money sucessfully')
+        const account = document.getElementById('balance').innerText;
+        console.log(account)
+
+        const addMoneyNumber = parseFloat(addMonyInput);
+        const balanceNumber = parseFloat(account);
+        const newBalance = addMoneyNumber + balanceNumber;
+        console.log(newBalance)
+
+        document.getElementById('balance').innerText = newBalance;
+        
+    } else {
+        alert("invalid number.please enter the positive number");
+    }
 
 });
